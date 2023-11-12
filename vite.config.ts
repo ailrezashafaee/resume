afd6b5d7ec5ff6f3d9f8dceb20105a1434085b2f
ailrezashafaee <alirezashafaee@aut.ac.ir>
2023-11-13 00:03:00 +0330

    vite config setud

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/resume",
   plugins: [react()],
-  server: {
-    port: 3006,
-  },
+  base:"/resume/"
 });
